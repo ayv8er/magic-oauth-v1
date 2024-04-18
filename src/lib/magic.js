f88c76414c0ce2c1bab786dd243f267f23177959
Richard Oh <richard@magic.link>
2024-04-17 23:16:44 -0500

    update title and magic constructor

--- src/lib/magic.js
+++ src/lib/magic.js
@@ -1,13 +1,6 @@
 import { Magic } from "magic-sdk";
-import { OAuthExtension } from "@magic-ext/oauth";
+import { OAuthExtension } from '@magic-ext/oauth';
 
-const createMagic = (key) => {
-  return (
-    typeof window !== "undefined" &&
-    new Magic(key, {
-      extensions: [new OAuthExtension()],
-    })
-  );
-};
-
-export const magic = createMagic("pk_live_4BB2713F29DC9392");
+export const magic = new Magic("pk_live_4BB2713F29DC9392", {
+    extensions: [new OAuthExtension()],
+});
